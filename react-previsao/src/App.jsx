9a27cfc23bb98f657354c05cdc556d2c162672cb
Guilherme Denobi <34285566+Denobi@users.noreply.github.com>
2024-08-17 19:59:43 -0300

    VersionFinal

--- react-previsao/src/App.jsx
+++ react-previsao/src/App.jsx
@@ -8,7 +8,9 @@ function App() {
   const [weather, setWeather] = useState()
   const [weatherFiveDays, setweatherFiveDays] = useState()
   const inputRef = useRef();
-
+  const clearInputs=()=>{
+    document.querySelector(".search-input").value="";
+  }
   async function searchCity(){
    const city = inputRef.current.value;
     const key = "e9a7f56406a66e38334695dd7a26373f";
@@ -25,7 +27,7 @@ function App() {
   return (
     <div className='container'>
      <h1>devClub Previsão</h1>
-     <input ref={inputRef} type="text" placeholder='Digite o nome da cidade'/>
+     <input className='search-input' ref={inputRef} type="text" onClick={clearInputs} placeholder='Digite o nome da cidade'/>
      <button onClick={searchCity} className='buttonSend'>Buscar</button>
       {weather && <WeatherInformations weather={weather}/>}
       {weatherFiveDays && <WeatherFiveDays weatherFiveDays={weatherFiveDays}/>}
